446512c42bf57ac1b8706e25edde1edae66f6807
churchill nnanna <mrxyfactor@gmail.com>
2025-11-04 12:18:04 +0100

    extracted projects-header from within projects comp.

---
+++ components/home/client/MyPortfolioHeader.tsx
@@ -0,0 +1,80 @@
+"use client";
+import Section from "@/components/common/Section";
+import { Button } from "@/components/ui/button";
+import { usePathname, useSearchParams } from "next/navigation";
+import {useEffect, useState} from 'react'
+import Search from "@/components/portfolio/client/Search";
+import { TProject } from "../types";
+
+const projectCategories = [
+  {
+    key: "all",
+    label: "All Projects",
+  },
+  {
+    key: "uiux",
+    label: "UIUX",
+  },
+  {
+    key: "graphic",
+    label: "Graphic Design",
+  },
+  {
+    key: "branding",
+    label: "Logo & Branding",
+  },
+];
+
+type TMyPortfolioHeader = {
+  setCategory: React.Dispatch<React.SetStateAction<string>>;
+  category: string;
+  setSubmitting: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function MyPortfolioHeader({
+  setCategory,
+  category,
+  setSubmitting,
+}: TMyPortfolioHeader) {
+  const pathname = usePathname();
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+      setIsClient(true);
+    }, []);
+
+  return (
+    <div className="flex flex-col gap-6 lg:gap-0 lg:flex-row justify-between items-center w-full mb-11">
+      {isClient && pathname != "/portfolio" && (
+        <Section
+          title="My Portfolio"
+          description="Showcasing my best work across different industries"
+          style="w-full"
+        />
+      )}
+      {isClient && pathname == "/portfolio" && (
+        <div className="">
+          <Search setSubmitting={setSubmitting} />
+        </div>
+      )}
+      <div className="flex flex-wrap lg:flex-nowrap justify-center  gap-1 gap-y-2 text-xs mb-6 lg:mb-0">
+        {projectCategories.map((cat, index) => (
+          <Button
+            key={cat.label}
+            variant={"outline"}
+            onClick={() => setCategory(cat.key)}
+            className={`${
+              category == cat.key || (index == 0 && !category)
+                ? "bg-btn-bg! hover:bg-btn-bg/90! text-white! hover:border-background!"
+                : "hover:border-btn-outline-border!"
+            } transition-colors duration-300 border-background! `}
+          >
+            {cat.label}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MyPortfolioHeader;
