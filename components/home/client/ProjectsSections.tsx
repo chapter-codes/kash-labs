1d14e021c7b87808fdb7e8ea527798e7a8e90ec2
churchill nnanna <mrxyfactor@gmail.com>
2025-11-04 12:16:55 +0100

    added projeccts section for portfolio

---
+++ components/home/client/ProjectsSections.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState, useEffect } from "react";
+import type { TProject } from "../types";
+import MyPortfolioHeader from "./MyPortfolioHeader";
+import Projects from "./Projects";
+type  TProjectsSection ={
+   inlineComponent? : React.ReactNode,
+   projects: TProject[] | undefined
+}
+
+function ProjectsSections({projects, inlineComponent }:TProjectsSection) {
+const [category, setCategory] = useState('all')
+const [newProjects, setNewProjects ] = useState<TProject[] | undefined>(projects)
+const [submitting, setSubmitting] = useState(false);
+
+
+useEffect(()=>{
+  setSubmitting(false)
+}, [projects])
+
+
+  return (
+    <>
+      <MyPortfolioHeader
+        category={category}
+        setCategory={setCategory}
+        // setNewProjects={setNewProjects}
+        setSubmitting={setSubmitting}
+      />
+      <section>
+        {inlineComponent}
+      </section>
+      <Projects
+        projects={projects}
+        category={category}
+        submitting={submitting}
+      />
+    </>
+  );
+}
+
+export default ProjectsSections
---
+++ components/portfolio/server/ProjectsLoader.tsx
@@ -0,0 +1,13 @@
+import { getProjects } from "@/lib/controllers/ProjectController";
+import Projects from "@/components/home/client/Projects";
+import ProjectsSections from "@/components/home/client/ProjectsSections";
+
+
+async function ProjectsLoader() {
+  const projects = await getProjects();
+
+  return  <ProjectsSections projects={projects} />;
+  
+}
+
+export default ProjectsLoader;
